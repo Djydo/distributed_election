86bfdd3d3ede5baeb7710896c9f08603b7056011
Djydo <dayoisrael@gmail.com>
2017-07-18 20:28:40 -0500

    Removed author and license metadata

--- nodes.py
+++ nodes.py
@@ -20,11 +20,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#
-#
 
-__author__ = "Djydo"
-__license__ = "GPL"
 
 import socket
 import sys
--- tracker.py
+++ tracker.py
@@ -21,8 +21,6 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
-__author__ = "Djydo"
-__license__ = "GPL"
 
 import socket
 import select
